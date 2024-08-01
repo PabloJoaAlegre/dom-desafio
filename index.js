2cb516e525b5ff68873b3b294b15cf2e3379284c
PabloJoaAlegre <161892012+PabloJoaAlegre@users.noreply.github.com>
2024-08-01 19:07:41 -0300

    index.js

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
+const cosasQueAprendimos = [
+  {
+    tema: "terminal",
+    class: "essential",
+  },
+  {
+    tema: "node",
+    class: "server",
+  },
+  {
+    tema: "oop",
+    class: "fundamental",
+  },
+  {
+    tema: "typescript",
+    class: "typed",
+  },
+  {
+    tema: "css",
+    class: "style",
+  },
+  {
+    tema: "dom",
+    class: "special",
+  },
+];
+
 function main() {}
     
-    const lista = document.getElementsByClassName("lista");
-    const items = lista.getElementsByTagName("li");
-    const itemsArray = Array.from(items)
-
-    itemsArray.forEach(item => lista.removeChild(item))
-
-    const cosasQueAprendimos = [
-      {
-        tema: "terminal",
-        class: "essential",
-      },
-      {
-        tema: "node",
-        class: "server",
-      },
-      {
-        tema: "oop",
-        class: "fundamental",
-      },
-      {
-        tema: "typescript",
-        class: "typed",
-      },
-      {
-        tema: "css",
-        class: "style",
-      },
-      {
-        tema: "dom",
-        class: "special",
-      },
-    ];
-
-    cosasQueAprendimos.forEach(cosas => {
-      const li = document.createElement("li");
-
-      li.textContent = cosas.tema;
-
-      if (cosas.class){
-        li.classList.add(cosa.class);
-      }
-
+    const lista = document.querySelector(".lista");
+    const items = lista.querySelectorAll("li");
+    items.forEach((elemento)=> {
+      elemento.remove();
+    });
+    cosasQueAprendimos.forEach((cosas) => {
+      let li = document.createElement("li");
+          li.textContent = cosas.tema;
+          li.classList.add(cosas.class || "item");
       lista.appendChild(li);
     })
-
     main();
 
